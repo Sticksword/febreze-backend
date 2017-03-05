9f56f7adfae20849c1e850fdb574051e00f4005f
Michael Chen <mzc2fd@virginia.edu>
2017-03-04 18:08:31 -0800

    fucking spent 2.5 hours fixing something that wasn't broken

--- routes/nest-controller.js
+++ routes/nest-controller.js
@@ -61,10 +61,43 @@ function makeTemperaturePost (temp, next) {
   request.put(options)
     .then(function (parsedBody) {
       // POST succeeded...
-      console.log('success!!!!');
+      console.log(parsedBody);
+      next(parsedBody);
     })
     .catch(function (err) {
       // POST failed...
+      if (err.statusCode === 307) {
+        redirect(temp, err.response.headers.location, next);
+      }
+      else {
+        next(err);
+      }
+    });
+}
+
+function redirect(temp, url, next) {
+  console.log(url);
+  var options = {
+    method: 'PUT',
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer c.ZzwnlUv6cmKsoYVjbMmL5v6umieg06MHmp5VRnhJ3ekxr7NwPmr7QVjKm8eLQNjFafiOc3FP59E51TUfJCJ8oNRmnFyPYnQtS2TIwTzAAANina6TdhZoGIunxBPJlsvNjAl1W6AWR16fpF7B',
+      'Content-Type': 'application/json'
+    },
+    body: {
+      'target_temperature_f': parseInt(temp)
+    },
+    json: true, // Automatically stringifies the body to JSON
+    // simple: false
+  };
+
+  request.put(options)
+    .then(function (parsedBody) {
+      console.log('redirect success');
+      next('hurray');
+    })
+    .catch(function (err) {
+      console.log('redirect error');
       next(err);
     });
 }
