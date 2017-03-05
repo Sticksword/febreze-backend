3307c8741272d9938ad38a2442a403ae22ecf9ae
Michael Chen <mzc2fd@virginia.edu>
2017-03-04 19:56:33 -0800

    copy pasta to fan controls

--- routes/nest-controller.js
+++ routes/nest-controller.js
@@ -17,6 +17,68 @@ router.post('/', function(req, res) {
   });
 });
 
+function makeTemperaturePost (duration, next) {
+  var options = {
+    method: 'PUT',
+    uri: 'https://developer-api.nest.com/devices/thermostats/XvVANsbisE06xwxNqVZNmqHfbGN0RAej',
+    headers: {
+      'Authorization': 'Bearer c.ZzwnlUv6cmKsoYVjbMmL5v6umieg06MHmp5VRnhJ3ekxr7NwPmr7QVjKm8eLQNjFafiOc3FP59E51TUfJCJ8oNRmnFyPYnQtS2TIwTzAAANina6TdhZoGIunxBPJlsvNjAl1W6AWR16fpF7B',
+      'Content-Type': 'application/json'
+    },
+    body: {
+      "fan_timer_active": true,
+      "fan_timer_duration": duration
+    },
+    json: true, // Automatically stringifies the body to JSON
+  };
+
+  request.put(options)
+    .then(function (parsedBody) {
+      // POST succeeded...
+      console.log(parsedBody);
+      next(parsedBody);
+    })
+    .catch(function (err) {
+      // POST failed...
+      if (err.statusCode === 307) {
+        redirect(duration, err.response.headers.location, next);
+      }
+      else {
+        next(err);
+      }
+    });
+}
+
+function redirect(temp, url, next) {
+  console.log(url);
+  var options = {
+    method: 'PUT',
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer c.ZzwnlUv6cmKsoYVjbMmL5v6umieg06MHmp5VRnhJ3ekxr7NwPmr7QVjKm8eLQNjFafiOc3FP59E51TUfJCJ8oNRmnFyPYnQtS2TIwTzAAANina6TdhZoGIunxBPJlsvNjAl1W6AWR16fpF7B',
+      'Content-Type': 'application/json'
+    },
+    body: {
+      "fan_timer_active": true,
+      "fan_timer_duration": parseInt(duration)
+    },
+    json: true, // Automatically stringifies the body to JSON
+    // simple: false
+  };
+
+  request.put(options)
+    .then(function (parsedBody) {
+      console.log('redirect success');
+      next('hurray');
+    })
+    .catch(function (err) {
+      console.log('redirect error');
+      next(err);
+    });
+}
+
+
+// COPIED AND PASTED
 
 function makeTemperaturePost (temp, next) {
   var options = {
