861cf495a75521d3bcd21b3a2b48842e839b3832
Michael Chen <mzc2fd@virginia.edu>
2017-03-04 19:46:01 -0800

    dealt with 307 crap

--- routes/nest-controller.js
+++ routes/nest-controller.js
@@ -1,14 +1,12 @@
 /*jshint esversion: 6 */
-
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'nest' });
 });
-
-
 
 router.post('/', function(req, res) {
   console.log('=========');
@@ -17,33 +15,10 @@ router.post('/', function(req, res) {
   makeTemperaturePost(req.body.temp, function(result) {
     res.send(result);
   });
-
 });
-
-// var request = require('request');
-var request = require('request-promise');
-
 
 
 function makeTemperaturePost (temp, next) {
-
-  // var options = {
-  //   method: 'PUT',
-  //   url: 'https://developer-api.nest.com/devices/thermostats/XvVANsbisE06xwxNqVZNmqHfbGN0RAej',
-  //   headers: {
-  //     authorization: 'Bearer c.nz0cGM30l2SqHokV7GS2RjTTph84DLLOqb3Fwdgcwe5aaXKDolffeu8gDS6hgS2HK8NgjqrjR8ZtuG5v5yqNdcEW2hwHFLgPLEA2wqzHc33SM8GcVhhJpliLQDYktKx2xKpZcdRiL3mt1Qgc',
-  //     'content-type': 'application/json'
-  //   },
-  //   body: { 'target_temperature_f': temp },
-  //   json: true
-  // };
-  //
-  // request(options, function (error, response, body) {
-  //   if (error) throw new Error(error);
-  //
-  //   console.log(body);
-  // });
-
   var options = {
     method: 'PUT',
     uri: 'https://developer-api.nest.com/devices/thermostats/XvVANsbisE06xwxNqVZNmqHfbGN0RAej',
@@ -55,7 +30,6 @@ function makeTemperaturePost (temp, next) {
       'target_temperature_f': temp
     },
     json: true, // Automatically stringifies the body to JSON
-    // simple: false
   };
 
   request.put(options)
